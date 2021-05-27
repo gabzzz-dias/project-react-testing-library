d972fefb672301d623e6bfd55ecc57d810d7e811
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-27 18:58:34 -0300

    request 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import App from '../App';
+
+test('if shows "no pokemon found" message if dont have favourite pokémons', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/favorites');
+
+  const anyPokemon = getByText(/no favorite pokemon found/i);
+  expect(anyPokemon).toBeInTheDocument();
+});
