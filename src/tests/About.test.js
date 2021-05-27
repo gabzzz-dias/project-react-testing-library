6e30eb21ccd79a37a070293e6bb5eb64925d3b0c
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-27 17:40:18 -0300

    request 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import About from '../components/About';
+
+const { MemoryRouter } = require('react-router');
+
+describe('if the page contains info about pokédex', () => {
+  test('if the page contains a h2 with the text About Pokédex', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const about = getByRole('link', { name: /about/i });
+    fireEvent.click(about);
+
+    const title = getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('if the page contains 2 paragraphs about Pokédex', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const paragraph = container.getElementsByTagName('p');
+    expect(paragraph[0]).toBeInTheDocument();
+    expect(paragraph[1]).toBeInTheDocument();
+  });
+
+  test('if the page contains the following image', () => {
+    const { getByAltText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const img = getByAltText('Pokédex');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
