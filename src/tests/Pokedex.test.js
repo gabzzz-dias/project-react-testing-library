22e76aa9af6b0b15e81463ccb1f6e340712d2881
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-28 00:02:49 -0300

    last commit today

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRoute from '../helper/renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Request 5', () => {
+  it('if contains a heading lvl 2 with following text', () => {
+    const { getByRole } = renderWithRoute(<Pokedex />);
+    const h2 = getByRole('heading', { level: 2, name: /Encountered pokémons/i });
+    expect(h2).toBeInTheDocument();
+  });
+});
