bbe683039f3719dbbff257290886f1050df88212
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-30 22:33:21 -0300

    request 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import App from '../App';
+import { fireEvent, screen } from '@testing-library/react';
 import renderWithRoute from '../helper/renderWithRoute';
+import App from '../App';
 
 describe('Request 6', () => {
-  describe('if the page render a card with current pokemon infos', () => {
-    it('', () => {
-      renderWithRoute(<App />);
-    });
+  const pkmName = 'pokemon-name';
+  const pkmType = 'pokemon-type';
+  const pkmWeight = 'pokemon-weight';
+
+  it('shows the card with info', () => {
+    renderWithRoute(<App />);
+
+    const name = screen.getByTestId(pkmName).textContent;
+    const type = screen.getByTestId(pkmType).textContent;
+    const weight = screen.getByTestId(pkmWeight).textContent;
+    const pkmImg = screen.getByAltText(/pikachu sprite/i);
+
+    expect(name).toBe('Pikachu');
+    expect(type).toBe('Electric');
+    expect(weight).toBe('Average weight: 6.0 kg');
+    expect(pkmImg).toBeInTheDocument();
+    expect(pkmImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('contains a details link', () => {
+    renderWithRoute(<App />);
+
+    const link = screen.getByText(/more details/i);
+    expect(link).toBeInTheDocument();
+
+    fireEvent.click(link);
+
+    const details = screen.getByText('Pikachu Details');
+    expect(details).toBeInTheDocument();
+  });
+
+  it('contains a star icon in favorite pokemons', () => {
+    renderWithRoute(<App />);
+
+    const link = screen.getByText(/more details/i);
+    fireEvent.click(link);
+
+    const favorite = screen.getByText(/pokémon favoritado/i);
+    expect(favorite).toBeInTheDocument();
+    fireEvent.click(favorite);
+
+    const star = screen.getByAltText(/pikachu is marked as favorite/i);
+    expect(star).toBeInTheDocument();
+    expect(star).toHaveAttribute('src', '/star-icon.svg');
   });
 });
